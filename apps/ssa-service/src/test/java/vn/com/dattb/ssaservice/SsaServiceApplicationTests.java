a14d7dd1fb59980841364502168acdaaa3d06bbc
Tran Ba Dat <dat.tb98it@gmail.com>
2024-12-13 21:52:26 +0700

    add: ssa - signing server application module

---
+++ apps/ssa-service/src/test/java/vn/com/dattb/ssaservice/SsaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.dattb.ssaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
