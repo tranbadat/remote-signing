d9f49f79bf36ed8fa58e4644a8f093a6632611cd
Tran Ba Dat <dat.tb98it@gmail.com>
2024-12-20 21:04:49 +0700

    add: challenge - define challenge service - commit challenge

---
+++ apps/ssa-service/src/main/java/vn/com/dattb/ssaservice/dto/response/SuccessResponse.java
@@ -0,0 +1,25 @@
+package vn.com.dattb.ssaservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * SuccessResponse
+ * <p>
+ * Author: Tran Ba Dat - <a href="https://econtract.dattb.com">Live demo</a>
+ * Created: 12/20/2024
+ * Version: 1.0.0
+ * <p>
+ * Description: DTO for success response
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class SuccessResponse {
+    private String code = "00";
+
+    private String message = "success";
+}
