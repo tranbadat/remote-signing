a47e2695900e3a12e9cb1ada750275bc3b0b0529
Tran Ba Dat <dat.tb98it@gmail.com>
2025-01-04 23:14:32 +0700

    refactor: ssa - add fcm to notification service

---
+++ apps/ssa-service/src/main/java/vn/com/dattb/ssaservice/service/NotificationService.java
@@ -0,0 +1,13 @@
+package vn.com.dattb.ssaservice.service;
+
+/**
+ * NotificationService
+ * <p>
+ * Author: Tran Ba Dat - <a href="https://econtract.dattb.com">Live demo</a>
+ * Created: 1/4/2025
+ * Version: 1.0.0
+ * <p>
+ * Description: Service for handling notification requests
+ */
+public interface NotificationService {
+}
