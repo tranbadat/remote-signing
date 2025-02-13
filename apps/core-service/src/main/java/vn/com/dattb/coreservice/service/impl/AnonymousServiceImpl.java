ff6af31b30e2aa320d883644fb986db4612c9a10
Tran Ba Dat <dat.tb98it@gmail.com>
2025-02-13 22:36:46 +0700

    feat: core-service - implementation class interface

---
+++ apps/core-service/src/main/java/vn/com/dattb/coreservice/service/impl/AnonymousServiceImpl.java
@@ -0,0 +1,19 @@
+package vn.com.dattb.coreservice.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import vn.com.dattb.coreservice.service.AnonymousService;
+
+/**
+ * AnonymousServiceImpl is the service class for anonymous service
+ * <p>
+ * Author: Tran Ba Dat - <a href="https://econtract.dattb.com">Live demo</a>
+ * Created: 2/13/2025
+ * Version: 1.0.0
+ * <p>
+ * Description: AnonymousServiceImpl is the service class for anonymous service
+ */
+@Service
+@Slf4j
+public class AnonymousServiceImpl implements AnonymousService {
+}
