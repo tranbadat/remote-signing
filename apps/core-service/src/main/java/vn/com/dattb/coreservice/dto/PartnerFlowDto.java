e5a82657a4ed9869851dab5748fcd13c6062e728
Tran Ba Dat <dat.tb98it@gmail.com>
2025-03-04 21:42:00 +0700

    feat: partner signing flow service

--- apps/core-service/src/main/java/vn/com/dattb/coreservice/dto/PartnerFlowDto.java
+++ apps/core-service/src/main/java/vn/com/dattb/coreservice/dto/PartnerFlowDto.java
@@ -2,21 +2,21 @@ package vn.com.dattb.coreservice.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import vn.com.dattb.coreservice.PartnerType;
 
 /**
- * SignerFlowDto
+ * PartnerFlowDto
  * <p>
  * Author: Tran Ba Dat - <a href="https://econtract.dattb.com">Live demo</a>
  * Created: 3/3/2025
  * Version: 1.0.0
  * <p>
- * Description: SignerFlowDto is the DTO class for signer flow
+ * Description: PartnerFlowDto is the DTO class for partner flow
  */
 @Getter
 @Setter
-public class SignerFlowDto {
+public class PartnerFlowDto {
     private Long id;
     private String name;
-    private String email;
-    private Long partnerId;
+    private PartnerType partnerType;
 }
